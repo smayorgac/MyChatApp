89d6a77eb79c9b8750b5fb2a14ac779397f49841
Sebastian <sebastian.mayogra@loblaw.ca>
2020-02-11 11:39:37 -0600

    Setting up structure

---
+++ MyChatApp/MyChatApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  MyChatApp
+//
+//  Created by Sebastian Mayorga on 2/11/20.
+//  Copyright © 2020 Sebastian Mayorga. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyChatApp/MyChatApp/Views/LoginView.swift
@@ -0,0 +1,13 @@
+//
+//  LoginView.swift
+//  MyChatApp
+//
+//  Created by Sebastian Mayorga on 2/11/20.
+//  Copyright © 2020 Sebastian Mayorga. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+}
